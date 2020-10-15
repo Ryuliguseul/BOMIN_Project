691458837f228e46668f685dc97685f9fb569eab
Ryuliguseul <48178460+Ryuliguseul@users.noreply.github.com>
2020-10-15 18:21:32 +0900

    로그인 페이지 구현

---
+++ app/src/main/java/com/example/bomin_project/LoginResultActivity.java
@@ -0,0 +1,26 @@
+package com.example.bomin_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginResultActivity extends AppCompatActivity {
+    TextView TextView_get;
+
+    protected void OnCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView_get = findViewById(R.id.TextView_get);
+
+        Intent intent = getIntent();
+
+        Bundle bundle = intent.getExtras();
+        String email = bundle.getString("email");
+        String password = bundle.getString("password");
+
+        TextView_get.setText(email + " / " + password)
+    }
+}
--- app/src/main/java/com/example/bomin_project/MainActivity.java
+++ app/src/main/java/com/example/bomin_project/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.bomin_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText TextInputEditText_email, TextInputEditText_password;
+    private Object View;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextInputEditText_email = findViewById(R.id.TextInputEditText_email);
+        TextInputEditText_password = findViewById(R.id.TextInputEditText_password);
+
+        public void onClick(View v){
+            String email = TextInputEditText_email.getText().toString();
+            String password = TextInputEditText_password.getText().toString();
+
+            Intent intent = new Intent(MainActivity.this, LoginResultActivity.class);
+            intent.putExtra("email", email);
+            intent.putExtra("password", password);
+            startActivity(intent);
+
+        }
     }
 }
